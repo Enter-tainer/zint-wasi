4afd4a7f02f28ea8a8319d4a0f6c2fdea85ac26a
mgt <mgt@oi-wiki.org>
2023-11-26 20:40:13 +0800

    Add LICENSE file to typst-package

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cargo build --release --target wasm32-wasi
 cargo about generate about.hbs > typst-package/license.html
 wasi-stub -r 0 ./target/wasm32-wasi/release/zint_typst_plugin.wasm -o typst-package/zint_typst_plugin.wasm
 wasm-opt typst-package/zint_typst_plugin.wasm -O3 --enable-bulk-memory -o typst-package/zint_typst_plugin.wasm
-
+cp LICENSE typst-package/LICENSE
 typst compile typst-package/manual.typ typst-package/manual.pdf
 typst compile typst-package/example.typ typst-package/example.svg
 typst compile typst-package/manual.typ 'assets/doc-{n}.svg'
