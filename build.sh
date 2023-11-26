f5667d86905e047a3183d611d8b6cdd09269934b
mgt <mgt@oi-wiki.org>
2023-11-26 16:55:40 +0800

    Add license.html and package.license to Cargo.toml

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
 cargo build --release --target wasm32-wasi
-# cargo about generate about.hbs > license.html
-# cp license.html typst-package/
+cargo about generate about.hbs > typst-package/license.html
 wasi-stub -r 0 ./target/wasm32-wasi/release/zint_typst_plugin.wasm -o typst-package/zint_typst_plugin.wasm
 wasm-opt typst-package/zint_typst_plugin.wasm -O3 --enable-bulk-memory -o typst-package/zint_typst_plugin.wasm
